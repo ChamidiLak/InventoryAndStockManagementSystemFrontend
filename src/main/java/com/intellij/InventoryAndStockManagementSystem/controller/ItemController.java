903ac74dfadddc6ab9f8f1e3795bb0bfb15b4507
Mevinu Methdam <mevinumethdam@yahoo.com>
2025-05-20 23:52:05 +0530

    Update item controller

--- src/main/java/com/intellij/InventoryAndStockManagementSystem/controller/ItemController.java
+++ src/main/java/com/intellij/InventoryAndStockManagementSystem/controller/ItemController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/items")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ItemController {
     private final ItemService itemService = new ItemService();
 
